cc5353fcbf7ed620eda0f32f7368267300e1e804
hitska <gobbler.kun@gmail.com>
2022-04-24 20:22:43 +0300

    Теперь не пытаемся перехватить данные, а просто чиним ссылки по таймеру каждый 5 секунд

--- synch-fix.user.js
+++ synch-fix.user.js
@@ -1,12 +1,11 @@
 // ==UserScript==
 // @name        Фикс ссылок синча
 // @description Производит денацификацию ссылок синча
-// @version     1
+// @version     1.1
 // @include     *syn-ch.com/*
 // @include     *syn-ch.ru/*
 // @author      Твоя мамаша
-// @run-at 			document-end
-// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js
+// @run-at      document-end
 // ==/UserScript==
 
 // ----------------------------------------------------------------------------
@@ -51,22 +50,4 @@ function fixContent() {
 fixContent()
 
 // Просто чиним всё по таймеру
-//setInterval(function() {fixContent()}, 5000);
-
-
-// ----------------------------------------------------------------------------
-// Всратый способ фильтровать всё что мы получаем по ajax, вроде работает, но...
-// "USE OF UNSAFEWINDOW IS INSECURE, AND IT SHOULD BE AVOIDED WHENEVER POSSIBLE" (ц)
-// ----------------------------------------------------------------------------
-if (!unsafeWindow.XMLHttpRequest.prototype.getResponseText) {
-	unsafeWindow.XMLHttpRequest.prototype.getResponseText = Object.getOwnPropertyDescriptor(unsafeWindow.XMLHttpRequest.prototype, 'responseText').get;
-}
-
-Object.defineProperty(unsafeWindow.XMLHttpRequest.prototype, 'responseText', {
-	get: exportFunction(function() {
-		var responseText = unsafeWindow.XMLHttpRequest.prototype.getResponseText.call(this);
-		return denazifyBrokenAddresses(responseText);
-	}, unsafeWindow),
-	enumerable: true,
-	configurable: true
-});
+setInterval(function() {fixContent()}, 5000);
